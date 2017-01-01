4464c24a9ef5dbf71244cef887cff45053705dcc
taewookimfardark <2012142087@yonsei.ac.kr>
2017-01-01 22:23:29 +0900

    tab bar 플러그인 추가

--- app/login/Login.js
+++ app/login/Login.js
@@ -1,15 +1,51 @@
 import React, {Component} from 'react';
 import {
   View,
-  Text
+  Text,
+  TextInput,
+  Button
 } from 'react-native';
 
-export default class Login extends Component {
+import {connect} from 'react-redux';
+
+import * as NavigationActionCreators from '../general/navigation/NavigationActionCreators';
+
+
+class Login extends Component {
   render() {
     return(
-      <View>
-        <Text>Login</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+        <TextInput
+          style={{
+            width: 100
+          }}/>
+        <TextInput
+          style={{
+          width: 100
+          }}/>
+        <Button title="login" onPress={e => {
+          this.props.login();
+        }}/>
       </View>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: () => {
+      dispatch(NavigationActionCreators.push({key: 'main', title: 'Main'}));
+    }
+  }
+};
+
+export default connect(undefined, mapDispatchToProps)(Login)
+
+
+
--- app/main/Main.js
+++ app/main/Main.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
 import {
   View,
-  Text
+  Text,
+  TouchableHighlight
 } from 'react-native';
+import ViewPager from 'react-native-viewpager';
+
 
 export default class Main extends Component {
   render() {
     return(
       <View>
-        <Text>Main</Text>
+        <View style={{
+          height: 40,
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}>
+          <TouchableHighlight style={{width: 60, alignItems: 'center'}}>
+            <Text>Albume</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={{width: 60, alignItems: 'center'}}>
+            <Text>Home</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={{width: 60, alignItems: 'center'}}>
+            <Text>Inbox</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     )
   }
